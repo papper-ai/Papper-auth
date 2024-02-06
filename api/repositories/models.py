73df29b5e907ec86afd268a51e56fc838c2c1204
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2024-02-06 16:29:56 +0500

    Remove history from user model

--- api/repositories/models.py
+++ api/repositories/models.py
@@ -1,6 +1,5 @@
 import uuid
 from sqlalchemy import DateTime, String, func
-from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 
@@ -15,4 +14,3 @@ class User(Base):
     password = mapped_column(String)
     created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
     uuid = mapped_column(String, default=str(uuid.uuid4()), nullable=False)
-    history = mapped_column(JSONB, default=[])
