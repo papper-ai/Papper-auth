8b3c39395f27e270c9d05bdbbe63f29ec7170f4c
PixelPantz <kuznec200456@gmail.com>
2024-02-11 14:58:48 +0500

    uuid uniq and default

--- api/repositories/models.py
+++ api/repositories/models.py
@@ -17,4 +17,4 @@ class User(Base):
     email = mapped_column(String, primary_key=True)
     password = mapped_column(String)
     created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
-    uuid = mapped_column(String, default=str(uuid.uuid4()), nullable=False)
+    uuid = mapped_column(String, default=uuid_default, nullable=False, unique=True)
