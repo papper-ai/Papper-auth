3b3055149d677ddb871f993210282c8a90cc4d96
PixelPantz <kuznec200456@gmail.com>
2024-03-14 21:49:49 +0500

    Change path to secrets

--- auth-service/src/config.py
+++ auth-service/src/config.py
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).parent
 
 
 class AuthJWT(BaseModel):
-    private_key_path: Path = BASE_DIR / "auth" / "certs" / "private.pem"
-    public_key_path: Path = BASE_DIR / "auth" / "certs" / "public.pem"
+    private_key_path: Path = BASE_DIR / "certs" / "private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "public.pem"
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 15
     refresh_token_expire_hours: int = 24
