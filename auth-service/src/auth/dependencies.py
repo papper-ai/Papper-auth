ecc744019a753b2f9bcce2aeefe61fe5458c12fe
PixelPantz <kuznec200456@gmail.com>
2024-03-15 19:15:38 +0500

    add_secret_depends depends

--- auth-service/src/auth/dependencies.py
+++ auth-service/src/auth/dependencies.py
@@ -5,7 +5,7 @@ import uuid
 from fastapi import Cookie, HTTPException, status, Form
 from pydantic import UUID4
 from auth import utils
-from auth.schemas import RegistrationRequest
+from auth.schemas import RegistrationRequest, Secret
 
 
 async def authentication_with_token(access_token: str = Cookie(alias="access-token")):
@@ -29,3 +29,8 @@ async def authentication_with_token(access_token: str = Cookie(alias="access-tok
 def registration_depends(secret: uuid.UUID = Form(...), name: str = Form(...), surname: str = Form(...), login: str = Form(...),
                          password: str = Form(...)):
     return RegistrationRequest(secret=secret, name=name, surname=surname, login=login, password=password)
+
+
+def add_secret_depends(secret: uuid.UUID = Form(..., description='17374954-9f59-4bdd-873f-9d30b95549bf'),
+                       created_by: str = Form(..., description='vasya')):
+    return Secret(secret=secret, created_by=created_by)
