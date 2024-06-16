2e1dc4574061e94be7397f27fb8ea8e2660e86f6
Mikhail Kuznecov <kuznecovmihail2019@gmail.com>
2024-06-16 13:25:40 +0300

    sqladmin implementation

--- auth-service/src/main.py
+++ auth-service/src/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from sqladmin import Admin
+
 from auth.router import auth_router
-import uvicorn
+from repositories.models import SecretsAdmin, UsersAdmin
+from repositories.postgres_repository import engine
 
 app = FastAPI()
 app.include_router(auth_router)
+
+admin = Admin(app, engine)
+admin.add_view(SecretsAdmin)
+admin.add_view(UsersAdmin)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
